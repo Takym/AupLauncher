574164a259d3d36dfdcc0f631f3776857f51ee67
Takym <yamato0818@live.jp>
2020-03-18 01:59:22 +0900

    プロジェクト ファイルを追加します。

---
+++ AupLauncher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AupLauncher
+{
+	public static class Program
+	{
+		public const string Author    = "Takym";
+		public const string Copyright = "Copyright (C) 2020 Takym.";
+		public const string Version   = "0.0.0.0";
+		public const string CodeName  = "aupl00a0";
+
+		[STAThread()]
+		internal static int Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new FormMain());
+			return 0;
+		}
+	}
+}
---
+++ AupLauncher/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle(nameof(AupLauncher))]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(AupLauncher.Program.Author)]
+[assembly: AssemblyProduct(nameof(AupLauncher))]
+[assembly: AssemblyCopyright(AupLauncher.Program.Copyright)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("c174ef44-fedc-42ce-92f7-41d969651530")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(AupLauncher.Program.Version)]
+[assembly: AssemblyFileVersion(AupLauncher.Program.Version)]
+[assembly: AssemblyInformationalVersion(AupLauncher.Program.CodeName)]
